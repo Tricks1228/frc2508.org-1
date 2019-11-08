d59c222254a234fd13a8acaabee269750d45356c
Ben <babbott1000@gmail.com>
2019-11-07 20:36:05 -0600

    Updated config.rb with pdfs dir

--- config.rb
+++ config.rb
@@ -5,6 +5,7 @@ set :theme, 'bootstrap-red'
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
+set :pdfs_dir, 'assets/pdfs'
 
 # Pretty URLs
 activate :directory_indexes
